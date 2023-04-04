f661d533d1b2703b4f669006d13525282c77c4dc
Hyder <hyderali400@gmail.com>
2023-04-04 10:13:39 +0530

    add features css and edit button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 const TodoItem = ({ itemProp, handleChange }) => {
+const [editing, setEditing] = useState(false);
 
     
-
+const handleEditing = () => {
+    setEditing(true);
+  };
 return (
     <li>
       <input
@@ -9,6 +13,7 @@ return (
         checked={itemProp.completed}
         onChange={() => handleChange(itemProp.id)}
       />
+       <button onClick={handleEditing}>Edit</button>
        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
       {itemProp.title}
     </li>
